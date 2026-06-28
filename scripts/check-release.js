fab0190540f74bcd2449aac89af823f1d1eddb4f
mustafazeydani <mustafamari20@gmail.com>
2026-06-28 16:47:14 +0300

    fix: tolerate pnpm argument delimiter in release check

--- scripts/check-release.js
+++ scripts/check-release.js
@@ -28,6 +28,10 @@ function parseArgs(argv) {
   for (let index = 0; index < argv.length; index += 1) {
     const arg = argv[index];
 
+    if (arg === '--') {
+      continue;
+    }
+
     if (!arg.startsWith('--')) {
       throw new Error(`Unexpected argument: ${arg}`);
     }
